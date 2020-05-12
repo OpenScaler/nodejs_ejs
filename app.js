480434c65690e08da7c9fd7058ea189d903fef2b
nechadi-king <nechadi.khaled@gmail.com>
2020-05-12 04:59:22 +0100

    add about page with CSS

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
 const express = require("express");
+const bodyParser= require("body-parser")
 const port = 3010;
-
 const app = express();
+
+var items=["pick Ibrakim"," bay milk","sport"];
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var today  = new Date();
+var currentDay = today.toLocaleDateString("en-US", options);
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("pubic"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tursday ";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thusday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-      default:
-          console.log(`Error current date is ${currentDay}`);
-  }
-  res.render("list", { kindOfDay: day, toDay: Date()});
+  
+  res.render("list", {toDay: currentDay , newitems:items});
 });
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.post("/",function(req,res){ 
+    var item = req.body.item;
+    items.push(item);
+    res.redirect("/");
+     console.log(item);   
+})
+
+app.get("/about",function(req,res){ res.render("about")});
+
 app.listen(port, () =>
   console.log(`Example app listening on port port! http://localhost:${port}`)
 );
